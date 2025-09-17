64a5fe98e1025d3027019f808f6b53e79a04d489
emoralesi <emoralesinzunza98@gmail.com>
2025-09-17 14:21:09 +0100

    agregar variables de entorno

--- src/db/ConnexionBD.js
+++ src/db/ConnexionBD.js
@@ -1,12 +1,16 @@
 import pkg from 'pg';
+import dotenv from 'dotenv';
 const { Client } = pkg;
 
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+dotenv.config({ path: envFile });
+
 const config = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'pr-custom-admin',
-  password: '123',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 };
 
 export function getClient() {
@@ -16,7 +20,7 @@ export function getClient() {
 export async function conectar(client) {
   try {
     await client.connect();
-    await client.query("SET search_path TO 'schema-admin';")
+    await client.query("SET search_path TO 'schema-admin';");
     console.log('Conexión establecida con PostgreSQL');
   } catch (error) {
     console.error('Error al conectar con PostgreSQL:', error);
